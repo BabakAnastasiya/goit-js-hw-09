c71ed421e45f85a9eab607929a7556a9d5065d0b
Babak Anastasiya <dp080791bap@gmail.com>
2022-01-04 13:56:41 +0200

    add 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const PROMPT_DELAY = 1000;
 
+const refs = {body : document.querySelector ('body'),
+startBtn : document.querySelector('button[data-start]'),
+stopBtn : document.querySelector('button[data-stop]'),
+};
+
+function changeBCgColor() {
+    let color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+}
+
+function startHandler () {
+    refs.startBtn.disabled = true;
+    intervalId = setInterval (changeBCgColor, PROMPT_DELAY);
+}
+
+function stopHandler (){
+    clearInterval (intervalId);
+    refs.startBtn.disabled = false;
+}
+
+refs.startBtn.addEventListener('click', startHandler)
+refs.stopBtn.addEventListener('click', stopHandler)
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
